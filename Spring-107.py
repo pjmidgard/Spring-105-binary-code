dd03cbedb148afbbd424d6381379ed07fd6e52b1
Java Software <pj.midgard@yahoo.com>
2022-06-20 09:56:32 +0300

    Create Spring-107.py

---
+++ Spring-107.py
@@ -0,0 +1,210 @@
+                                if assxw>=e3%2:
+                                           
+                                               
+
+                                            if e4=="1" and e3== e3%2:
+                                                sda3=sda3+"0"
+                                                e4="1"
+                                                e4=""
+                                                
+                                            elif e4=="0" and e3== e3%2:
+                                                sda3=sda3+"1"
+                                                e4="1"
+                                                e4=""                                                    
+                                            elif e4=="1":
+                                                    sda3=sda3+"0"
+                                                    e4="0"
+                                                    e4=""
+                                            elif e4=="1" and e3== e3%3:
+                                                    sda3=sda3+"0"
+                                                    e4="0"
+                                                    e4=""
+                                            elif e4=="0" and e3== e3%3:
+                                                    sda3=sda3+"1"
+                                                    e4="0"
+                                                    e4=""   
+                                            elif e4=="1" and e3== e3%4:
+                                                    sda3=sda3+"1"
+                                                    e4="0"
+                                                    e4=""
+                                            elif e4=="0" and e3== e3%4:
+                                                    sda3=sda3+"0"
+                                                    e4="0"
+                                                    e4=""                                                             
+                                            elif e4=="0" and e3== e3%5:
+                                                    sda3=sda3+"1"
+                                                    e4="0"
+                                                    e4=""   
+                                                                           
+                                            elif e4=="1" and e3== e3%5:
+                                                    sda3=sda3+"0"
+                                                    e4="0"
+                                                    e4=""
+                                                    
+                                            elif e4=="0" and e3== e3%6:
+                                                    sda3=sda3+"0"
+                                                    e4="0"
+                                                    e4=""   
+                                                                           
+                                            elif e4=="1" and e3== e3%6:
+                                                    sda3=sda3+"1"
+                                                    e4="0"
+                                                    e4=""                                                                                    
+                                            elif e4=="0" and e3== e3%7:
+                                                    sda3=sda3+"1"
+                                                    e4="0"
+                                                    e4=""   
+                                                                           
+                                            elif e4=="0" and e3== e3%7:
+                                                    sda3=sda3+"1"
+                                                    e4="0"
+                                                    e4=""    
+                                            elif e4=="0" and e3== e3%8:
+                                                    sda3=sda3+"1"
+                                                    e4="0"
+                                                    e4=""   
+                                                                           
+                                            elif e4=="0" and e3== e3%8:
+                                                    sda3=sda3+"1"
+                                                    e4="0"
+                                                    e4=""    
+                                            elif e4=="0" and e3== e3%9:
+                                                    sda3=sda3+"0"
+                                                    e4="0"
+                                                    e4=""   
+                                                                           
+              
+                                            elif e4=="1" and e3== e3%9:
+                                                    sda3=sda3+"1"
+                                                    e4="0"
+                                                    e4=""                                                                                                                                                                                                                                                                                                                                                        
+                                            elif e4=="0" and e3== e3%11:
+                                                    sda3=sda3+"1"
+                                                    e4="0"
+                                                    e4=""   
+                                                                           
+                                            elif e4=="0" and e3== e3%11:
+                                                    sda3=sda3+"1"
+                                                    e4="0"
+                                                    e4=""      
+                                        
+
+                                
+                                if e4=="1" and e3== e3%9+assxw:
+                                	sda3=sda3+"0"
+                                	e4="0"
+                                	e4=""
+                                
+                                elif e4=="1" and e3== e3%8+assxw:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""
+                                elif e4=="1" and e3== e3%7+sw2:
+                                	sda3=sda3+"0"
+                                	e4="0"
+                                	e4=""
+                                    
+                                elif e4=="1" and e3== e3%8+assxw:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""
+                                elif e4=="1" and e3== e3%7+sw2:
+                                	sda3=sda3+"0"
+                                	e4="0"
+                                	e4=""
+                                    
+                                elif e4=="1" and e3== e3%6+sw2:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""   
+                                
+                                elif e4=="1" and e3== e3%7+sw1:
+                                	sda3=sda3+"0"
+                                	e4="0"
+                                	e4=""
+                                    
+                                elif e4=="1" and e3== e3%6+sw1:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""
+                                elif e4=="1" and e3== e3%7+sw:
+                                	sda3=sda3+"0"
+                                	e4="0"
+                                	e4=""
+                                    
+                                elif e4=="1" and e3== e3%6+sw:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""
+                                
+                                elif e4=="1" and e3== e3%5+sw:
+                                	sda3=sda3+"0"
+                                	e4="0"
+                                	e4=""
+                                elif e4=="1" and e3== e3%2+sw7:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4="" 
+                                elif e4=="1" and e3== e3%2+sw6:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4="" 
+                                
+                                elif e4=="1" and e3== e3%18+sw6:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""	
+                                elif e4=="1" and e3== e3%18+sw6:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""
+                                elif e4=="1" and e3== e3%17+sw6:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""
+                                    
+   
+                                elif e4=="1" and e3== e3%4+sw4:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""
+                                    
+                           
+                                elif e4=="1" and e3== e3%3+sw4:
+                                	sda3=sda3+"0"
+                                	e4="0"
+                                	e4=""                                  
+                                elif e4=="1" and e3== e3%4+sw:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""
+                                    
+                           
+                                elif e4=="1" and e3== e3%3+sw:
+                                	sda3=sda3+"0"
+                                	e4="0"
+                                elif e4=="1" and e3== e3%23+n or e4=="1" and e3== e3%23+n1 or e4=="1" and e3== e3%23+n2:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""	
+                                 
+                                elif e4=="1" and e3== e3%22+n1:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""
+                                                                        
+                                elif e4=="1" and e3== e3%2+sw:
+                                    sda3=sda3+"0"
+                                    e4="1"
+                                    e4=""
+                                    
+                                        
+                                elif e4=="1":
+                                	sda3=sda3+"0"
+                                	e4="0"
+                                	e4=""
+                                    
+                                elif e4=="0":
+                                    sda3=sda3+"1"
+                                    e4="1"
+                                    e4="" 
